ad97a7ae77a914e97f964f0917e60c2e68a9642c
hafizur <coderpickbd@gmail.com>
2022-10-01 23:00:52 +0600

    Ep-03-pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+     <h1 class="bg-green-100 text-green-800 p-4">Hello,{{ name }}</h1>
+        <p>In this series, We will use the following frameworks</p>
+        <ul>
+            <li v-for="framework of frameworks" v-text="framework"></li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default{
+        props: {
+            name:String,
+            frameworks:Array
+        }
+    }
+</script>
+
